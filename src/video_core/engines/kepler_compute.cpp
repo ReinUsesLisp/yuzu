ef162d055bf0ad60712987f8b55e2e75eb3e0ede
ReinUsesLisp <reinuseslisp@airmail.cc>
2019-04-15 18:03:10 -0300

    TRY

--- src/video_core/engines/kepler_compute.cpp
+++ src/video_core/engines/kepler_compute.cpp
@@ -7,6 +7,8 @@
 #include "video_core/engines/kepler_compute.h"
 #include "video_core/memory_manager.h"
 
+#pragma optimize("", off)
+
 namespace Tegra::Engines {
 
 KeplerCompute::KeplerCompute(MemoryManager& memory_manager) : memory_manager{memory_manager} {}
@@ -19,12 +21,26 @@ void KeplerCompute::CallMethod(const GPU::MethodCall& method_call) {
 
     regs.reg_array[method_call.method] = method_call.argument;
 
+    LOG_CRITICAL(HW_GPU, "{:x} {:x}", method_call.method * 4, method_call.argument);
+
     switch (method_call.method) {
-    case KEPLER_COMPUTE_REG_INDEX(launch):
+    case KEPLER_COMPUTE_REG_INDEX(dest_address.high_dest_address):
+    case KEPLER_COMPUTE_REG_INDEX(dest_address.low_dest_address):
+        upload_cursor = 0;
+        break;
+    case KEPLER_COMPUTE_REG_INDEX(upload_data):
+        memory_manager.Write<u32>(regs.dest_address.Address() + upload_cursor,
+                                  method_call.argument);
+        upload_cursor += sizeof(u32);
+        break;
+    case KEPLER_COMPUTE_REG_INDEX(launch): {
         // Abort execution since compute shaders can be used to alter game memory (e.g. CUDA
         // kernels)
+        const auto cpu_addr = memory_manager.GpuToCpuAddress(regs.code_address.Address());
+        const auto pointer = memory_manager.GetPointer(regs.code_address.Address());
         UNREACHABLE_MSG("Compute shaders are not implemented");
         break;
+    }
     default:
         break;
     }
--- src/video_core/engines/kepler_compute.h
+++ src/video_core/engines/kepler_compute.h
@@ -31,11 +31,39 @@ public:
 
         union {
             struct {
-                INSERT_PADDING_WORDS(0xAF);
+                INSERT_PADDING_WORDS(0x62);
+
+                struct {
+                    u32 high_dest_address;
+                    u32 low_dest_address;
+
+                    GPUVAddr Address() const {
+                        return (static_cast<GPUVAddr>(high_dest_address) << 32) |
+                               static_cast<GPUVAddr>(low_dest_address);
+                    }
+                } dest_address;
+
+                INSERT_PADDING_WORDS(9);
+
+                u32 upload_data;
+
+                INSERT_PADDING_WORDS(0x41);
 
                 u32 launch;
 
-                INSERT_PADDING_WORDS(0xC48);
+                INSERT_PADDING_WORDS(0x4D2);
+
+                struct {
+                    u32 high_code_address;
+                    u32 low_code_address;
+
+                    GPUVAddr Address() const {
+                        return (static_cast<GPUVAddr>(high_code_address) << 32) |
+                               static_cast<GPUVAddr>(low_code_address);
+                    }
+                } code_address;
+
+                INSERT_PADDING_WORDS(0x774);
             };
             std::array<u32, NUM_REGS> reg_array;
         };
@@ -48,13 +76,18 @@ public:
 
 private:
     MemoryManager& memory_manager;
+
+    std::size_t upload_cursor{};
 };
 
 #define ASSERT_REG_POSITION(field_name, position)                                                  \
     static_assert(offsetof(KeplerCompute::Regs, field_name) == position * 4,                       \
-                  "Field " #field_name " has invalid position")
+                  "Field " #field_name " has invalid position");
 
+ASSERT_REG_POSITION(dest_address, 0x62);
+ASSERT_REG_POSITION(upload_data, 0x6D);
 ASSERT_REG_POSITION(launch, 0xAF);
+ASSERT_REG_POSITION(code_address, 0x582);
 
 #undef ASSERT_REG_POSITION
 
