6dc7c9ddfbed31f46700322da7b3482e1959d329
Fernando Sahmkow <fsahmkow27@gmail.com>
2020-02-19 13:40:37 -0400

    FenceManager: Manage syncpoints and rename fences to semaphores.

--- src/video_core/buffer_cache/buffer_cache.h
+++ src/video_core/buffer_cache/buffer_cache.h
@@ -231,6 +231,13 @@ public:
         return true;
     }
 
+    bool HasUncommitedFlushes() {
+        if (uncommited_flushes) {
+            return true;
+        }
+        return false;
+    }
+
     void PopAsyncFlushes() {
         if (commited_flushes.empty()) {
             return;
